c647b76da88d94a381cdda2735f2671f153ec265
nalinih <55223197+nalinih@users.noreply.github.com>
2019-12-11 20:12:05 +0530

    tesyt

---
+++ Prog/src/com/java/Test.java
@@ -0,0 +1,16 @@
+package com.java;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String value="dhanvi narumachi";
+		String[] str =value.split("");
+		for(int i=str.length-1;i>=0;i--) {
+			System.out.print(str[i]);
+		}
+
+
+	}
+
+}
